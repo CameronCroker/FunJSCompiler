3e7fb0cf0f4f8d7361a020398cbf5519de156e2c
Cameron Croker <cameron.croker@students.mq.edu.au>
2018-04-01 00:56:55 +1100

    Upload Of Project
    Initial upload of project as sample code

---
+++ src/funjs/Translator.scala
@@ -0,0 +1,116 @@
+package funjs
+
+/**
+ * Translator from funjs source programs to SEC target programs.
+ */
+object Translator {
+
+    import SECTree._
+    import FunJSTree._
+    import scala.collection.mutable.ListBuffer
+    //import SemanticAnalysis.{entity, tipe}
+    
+    /**
+     * Return a frame that represents the SEC instructions for a funjs program.
+     */
+    def translate (program : Program) : Frame = {
+
+        // An instruction buffer for accumulating the program instructions
+        val programInstrBuffer = new ListBuffer[Instr] ()
+
+        /**
+         * Translate the program by translating its expression.
+         */
+        val expInstrs = translateExpression (program.exp)
+        programInstrBuffer.appendAll (expInstrs)
+        programInstrBuffer.append (IPrint ())
+
+        // Gather the program's instructions and return them
+        programInstrBuffer.result ()
+
+    }
+    
+    /**
+     * Translate an expression and return a list of the instructions that
+     * form the translation.
+     */
+    def translateExpression (exp : Exp) : Frame = {
+        exp match {
+
+            // Simple expressions. Each of these just corresponds to a single
+            // instruction that uses a value from the source tree.
+
+            case BoolExp (value) =>
+                List (IBool (value))
+
+            case IdnUse (i) =>
+                List (IVar (i))
+
+            case IntExp (value) =>
+                List (IInt (value))
+
+            // Application, relational expressions and arithmetic expressions. Generate
+            // code to translate the two operand expressions and then generate the
+            // appropriate instruction to perform the operation.
+
+            case AppExp (l, r) =>
+                (translateExpression (l) ++ translateExpression (r)) :+ (ICall ())
+
+            case EqualExp (l, r) =>
+                (translateExpression (l) ++ translateExpression (r)) :+ (IEqual ())
+
+            case LessExp (l, r) =>
+                (translateExpression (l) ++ translateExpression (r)) :+ (ILess ())
+
+            case MinusExp (l, r) =>
+                (translateExpression (l) ++ translateExpression (r)) :+ (ISub ())
+
+            case PlusExp (l, r) =>
+                (translateExpression (l) ++ translateExpression (r)) :+ (IAdd ())
+
+            case SlashExp (l, r) =>
+                (translateExpression (l) ++ translateExpression (r)) :+ (IDiv ())
+
+            case StarExp (l, r) =>
+                (translateExpression (l) ++ translateExpression (r)) :+ (IMul ())
+
+            // An if expression translate into the code for the condition and a
+            // branch instruction containing the code for the left and right
+            // sides of the expression.
+
+            case IfExp (c, l, r) =>
+                (translateExpression (c)) :+ (IBranch (translateExpression (l), translateExpression (r)))
+
+            case FunExp (IdnDef (name, _), body) => 
+                List (IClosure (name, translateExpression (body) :+ IPopEnv ()))
+
+// FIXME
+            case WhereExp(e, List()) =>
+               translateExpression(e) 
+            case WhereExp(e, (Defn(IdnDef(idn,tipe), body))::ds) =>
+                translateExpression(AppExp(FunExp(IdnDef(idn,tipe),WhereExp(e,ds)),body))
+
+            case ObjExp(ds)=>
+            val defs = ds.collect {case Defn (IdnDef(i,t), e) => (i -> e)}
+                                           var map = Map[Identifier, Frame]()
+                                           defs.foreach {case (i, e) => 
+                                               map = map + (i -> translateExpression(e))
+                                                  }
+            List(IObj(map))
+
+            case DeRefExp(IdnUse(i), name) =>
+            List(IDeRef(i,name))
+                                              
+                                              
+
+                    
+                
+
+            
+
+
+
+        }
+    }
+
+}
